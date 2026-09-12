368d2c680705b311bfcc8b4890915c1c583a9db4
Mibess <mibexzooo@gmail.com>
2026-09-11 23:28:23 -0300

    Update game audio

--- src/systems/AudioSystem.ts
+++ src/systems/AudioSystem.ts
@@ -15,7 +15,7 @@ export class AudioSystem {
   private nextStepTime = 0;
   private stepIndex = 0;
   private isMusicPlaying = false;
-  private musicVolume = 0.38;
+  private musicVolume = 0.239;
 
   unlock() {
     if (!this.ctx) {
