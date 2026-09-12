cd006735892dc8cdf7f9e789ebc42068d912ef42
Mibess <mibexzooo@gmail.com>
2026-09-11 23:35:28 -0300

    Reduce music volume

--- src/systems/AudioSystem.ts
+++ src/systems/AudioSystem.ts
@@ -15,7 +15,7 @@ export class AudioSystem {
   private nextStepTime = 0;
   private stepIndex = 0;
   private isMusicPlaying = false;
-  private musicVolume = 0.239;
+  private musicVolume = 0.1195;
 
   unlock() {
     if (!this.ctx) {
